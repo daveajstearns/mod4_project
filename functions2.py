d950d6f519bea5f167d701438946a1fad31e3fec
Dave Stearns <daveajstearns@gmail.com>
2020-05-03 19:34:01 -0400

    functions file

---
+++ functions2.py
@@ -0,0 +1,53 @@
+def clean_drop(data):
+    """Provide data frame in parenthesis and this function will
+        drop nulls permanently, and reset the index."""
+    data.dropna(inplace=True)
+    data.reset_index(drop=True, inplace=True)
+    return data
+
+def reset_index(data):
+    """Provide data frame in parenthesis and this function will
+        reset your index."""
+    data.reset_index(drop=True, inplace=True)
+    return data
+
+def make_value_chart(data, column, x_label, y_label, title, color='PRGn'):
+    """Must fill out all arguments EXCEPT color. Color has a default setting."""
+    fig, ax = plt.subplots(figsize=(14,8))
+    ax = sns.barplot(x=list(data[column].value_counts().keys()),
+     y=data[column].value_counts(), palette=color)    
+    ax.set_xticklabels(ax.get_xticklabels(),
+                      rotation=45, horizontalalignment='right')
+    plt.xlabel(x_label, size=20)
+    plt.ylabel(y_label, size=20)
+    plt.title(title, size=26)
+    return plt.show()
+
+def rename_column(data, column, new_name):
+    """Quickly renames columns"""
+    data.rename(columns={column: new_name}, inplace=True)
+    return data
+
+def drop(data, columns=[]):
+    """Quickly drops columns"""
+    data.drop(columns=columns, inplace=True)
+    return data
+
+def dummy_up(data, column, prefix):
+    """Quickly makes dummies for the selected column"""
+    new = pd.get_dummies(data[column], prefix=prefix, drop_first=True)
+    return new
+
+def factorize(data, column):
+    """Factorizes categorical columns to prepare for RandomForestClassifier.  
+    Must apply to each column individually."""
+    new = pd.factorize(data[column])[0] + 1
+    return new
+
+def class_model(data, target, num_iter=5, models=['rfc', 'knn'], complexity='simple',
+                degree=2, graph=[''], fix_cb=[False], cv=3):
+    """This function will receive different user-defined commands (unless left blank, where there
+    some default settings), and generate models from the data.
+        * This will perform a cross validation on the data. The cv is customizable with a 
+        default of 3.
+        * 
